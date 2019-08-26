78ad05384e98bbfa75345f5b8d1d27916c794f43
1w1ng <921754984@qq.com>
2019-08-26 16:03:25 +0800

    引入 fastclick

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/index';
+
+// 1.引入 FastClick
+import FastClick from 'fastclick';
+if ('addEventListener' in document) {
+  document.addEventListener(
+    'DOMContentLoaded',
+    function() {
+      FastClick.attach(document.body);
+    },
+    false
+  );
+}
 
 new Vue({
+  router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
